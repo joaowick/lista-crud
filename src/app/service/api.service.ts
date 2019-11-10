d2959d46d03aeb4830cba4290e95bcce8b87a899
Joao Torres <joaotorrez14@gmail.com>
2019-11-10 19:18:18 -0300

    Add CRUD Services

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { Produto } from '../modal/produto';
+import { tap, catchError } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -16,10 +18,49 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  getProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(apiUrl)
+      .pipe(
+        tap(produtos => console.log('leu os produtos')),
+        catchError(this.handleError('getProdutos', []))
+      );
+  }
+
+  getProduto(id: number): Observable<Produto> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get<Produto>(url).pipe(
+      tap(_ => console.log(`leu o produto id=${id}`)),
+      catchError(this.handleError<Produto>(`getProduto id=${id}`))
+    );
+  }
+
+  addProduto (produto): Observable<Produto> {
+    return this.http.post<Produto>(apiUrl, produto, httpOptions).pipe(
+      tap((produto: Produto) => console.log(`adicionou o produto com w/ id=${produto._id}`)),
+      catchError(this.handleError<Produto>(`addProduto`))
+    );
+  }
+
+  updateProduto(id, produto): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, produto, httpOptions).pipe(
+      tap(_ => console.log(`atualiza o produto com id=${id}`)),
+      catchError(this.handleError<any>(`updateProuto`))
+    );
+  }
+
+  deleteProduto (id): Observable<Produto> {
+    const url = `${apiUrl}/delete/${id}`
+    return this.http.delete<Produto>(url, httpOptions).pipe(
+      tap(_ => console.log(`remove o produto com id = ${id}`)),
+      catchError(this.handleError<Produto>(`deleteProduto`))
+    );
+  }
+
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       return of(result as T);
-    }
+    };
   }
 }
