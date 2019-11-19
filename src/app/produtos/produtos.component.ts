ab85e6d3f8eb2245c8a8f45c8d383322195cf7e3
Joao Torres <joaotorrez14@gmail.com>
2019-11-19 11:57:01 -0300

    add service api no produto

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from 'src/model/produto';
+import { ApiService } from '../service/api.service'
 
 @Component({
   selector: 'app-produtos',
@@ -8,9 +9,22 @@ import { Produto } from 'src/model/produto';
 })
 export class ProdutosComponent implements OnInit {
 
-  constructor() { }
+  displayColumns: string[] = ['nome', 'desc', 'preco', 'acao'];
+  dataSource: Produto[];
+  isLoadingResults: boolean;
+
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
+    this._api.getProdutos()
+      .subscribe(res => {
+        this.dataSource = res;
+        console.log(this.dataSource);
+        this.isLoadingResults = false;
+      }, err => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
